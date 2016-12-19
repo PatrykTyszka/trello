e8a87b51a320e4748c62d61385190ff9161c90d6
Patryk Tyszka <patryk@codequest.com>
2016-12-19 22:22:09 +0100

    Encrypt password

--- web/models/user.ex
+++ web/models/user.ex
@@ -8,4 +8,21 @@ defmodule Trello.User do
 
     timestamps
   end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(username password))
+    |> validate_length(:username, min: 1, max: 20)
+    |> validate_length(:password, min: 8, max: 20)
+    |> encrypt_password()
+  end
+
+  defp encrypt_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
 end
